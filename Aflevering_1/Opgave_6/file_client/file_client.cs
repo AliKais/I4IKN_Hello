2bb6d2f1aa616fbe822edc2404e6a028b3cebdd9
Kaison <alikais98@gmail.com>
2019-02-27 23:30:02 +0100

    Tilføjet kommentarer

--- Aflevering_1/Opgave_6/file_client/file_client.cs
+++ Aflevering_1/Opgave_6/file_client/file_client.cs
@@ -15,16 +15,34 @@ namespace tcp
 		/// The BUFSIZE.
 		/// </summary>
 		const int BUFSIZE = 1000;
-
 		/// <summary>
 		/// Initializes a new instance of the <see cref="file_client"/> class.
 		/// </summary>
 		/// <param name='args'>
 		/// The command-line arguments. First ip-adress of the server. Second the filename
 		/// </param>
+
 		private file_client (string[] args)
 		{
-			// TO DO Your own code
+			try
+			{
+				Console.WriteLine(Connecting);
+				var client = new Tcpclient();
+				client.Connect(args[0], PORT)
+
+				var stream = client.GetStream();
+
+				LIB.writeTextTCP(stream, args[1]);
+				ReceiveFile(args[1], stream)
+			}
+			catch ()
+			{
+
+			}
+			finally
+			{
+
+			}
 		}
 
 		/// <summary>

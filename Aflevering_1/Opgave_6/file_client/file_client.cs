96b29123f5cd19d00f76854ab9a597bedbaee675
Kaison <alikais98@gmail.com>
2019-03-14 11:51:34 +0100

    Ændringer på client

--- Aflevering_1/Opgave_6/file_client/file_client.cs
+++ Aflevering_1/Opgave_6/file_client/file_client.cs
@@ -7,50 +7,32 @@ namespace tcp
 {
 	class file_client
 	{
-		/// <summary>
-		/// The PORT.
-		/// </summary>
 		const int PORT = 9000;
-		/// <summary>
-		/// The BUFSIZE.
-		/// </summary>
 		const int BUFSIZE = 1000;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="file_client"/> class.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments. First ip-adress of the server. Second the filename
-		/// </param>
 		private file_client (string[] args)
 		{
-			// TO DO Your own code
-           
-			Console.WriteLine(Connecting);
-			var client = new Tcpclient();
+			Console.WriteLine("Connecting");
+
+            TcpClient client = new TcpClient();
 			client.Connect(args[0], PORT);
 
-			var stream = client.GetStream();
+            NetworkStream stream = client.GetStream();
 
-			LIB.writeTextTCP(stream, args[1]);
+            Console.WriteLine("File requested");
+
+            LIB.writeTextTCP(stream, args[1]);
 			receiveFile(args[1], stream);
-			client.close();
-			stream.close();
 
+            Console.WriteLine("File received");
+
+            stream.Close();
+            client.Close();
 		}
 
-		/// <summary>
-		/// Receives the file.
-		/// </summary>
-		/// <param name='fileName'>
-		/// File name.
-		/// </param>
-		/// <param name='io'>
-		/// Network stream for reading from the server
-		/// </param>
+
 		private void receiveFile (String fileName, NetworkStream io)
 		{
-			// TO DO Your own code
 			//Find fil størrelsen
 			long Size_f = LIB.getFileSizeTCP(io);
 			if(Size_f<1)
@@ -72,12 +54,7 @@ namespace tcp
 			fs.Close();
 		}
 
-		/// <summary>
-		/// The entry point of the program, where the program control starts and ends.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments.
-		/// </param>
+
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Client starts...");

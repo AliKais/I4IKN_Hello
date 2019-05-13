34809a56dbf97246368def79c05132d5d67f5375
Kaison <alikais98@gmail.com>
2019-05-13 12:34:06 +0200

    små ændringer i klient

--- Aflevering_2/file_client/file_client.cs
+++ Aflevering_2/file_client/file_client.cs
@@ -13,44 +13,40 @@ namespace Application
 
 	    private file_client(String[] args)
 	    {
-            var fileSize    = 0;
-            var transport   = new Transport(BUFSIZE);
-            var buf         = new byte[BUFSIZE];
-            var filePath    = args[0];
+	        var fileSize    = 0;
+	        var transport   = new Transport(BUFSIZE);
+	        var buf         = new byte[BUFSIZE];
+	        var filePath    = args[0];
 
-            Console.WriteLine("Anmodning om fil fra server");
+	        Console.WriteLine("Anmodning om fil fra server");
 
-            transport.Send(Encoding.UTF8.GetBytes(filePath), filePath.Length);
-            var size = transport.Recive(ref buf);
+	        transport.Send(Encoding.UTF8.GetBytes(filePath), filePath.Length);
+	        var size = transport.Recive(ref buf);
 
-            if (size != 0)
-            {
-                fileSize = int.Parse(Encoding.UTF8.GetString(buf, 0, size));
-
-                if (fileSize > 0)
-                {
-                    receiveFile(filePath, transport);
-                }
-            }
+	        if ((size != 0) && (fileSize > 0))
+	        {
+	            fileSize = int.Parse(Encoding.UTF8.GetString(buf, 0, size));
+	            receiveFile(filePath, transport);
+	        }
 	    }
 
 		private void receiveFile (String fileName, Transport transport)
 		{
-            var read        = 0;
-            var readSize    = 0;
-            var filebuf     = new byte[BUFSIZE];
+			var read        = 0;
+      var readSize    = 0;
+      var filebuf     = new byte[BUFSIZE];
 			var fileName    = LIB.extractFileName(path);
 			var newFile     = new FileStream(fileName, FileMode.Create, FileAccess.Write);
 
-            Console.WriteLine("Modtager fil");  
+      Console.WriteLine("Modtager fil");
 
-            while (read < fileSize && (readSize = transport.Receive(ref fileBuf)) > 0) {
-		        newFile.Write(fileBuf, 0, readSize);
-		        read += readSize;
-		    }
+      while ((read < fileSize) && ((readSize = transport.Receive(ref fileBuf)) > 0)) {
+				newFile.Write(fileBuf, 0, readSize);
+		    read += readSize;
+			}
 
-		    if (read == fileSize){
-		        Console.WriteLine("Den ønskede fil blev modtaget");
+		  if (read == fileSize){
+				Console.WriteLine("Den Ã¸nskede fil blev modtaget");
 			}
 
 			newFile.Close();
